a07de0faa9c3a5e191c21ee2022bef291fe86364
Matheus Gonçalves <matheus.g2003@hotmail.com>
2023-09-14 19:42:18 -0300

    init repo

---
+++ src/main/java/br/com/heinzenberg/heinzenberg/HeinzenbergApplication.java
@@ -0,0 +1,13 @@
+package br.com.heinzenberg.heinzenberg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeinzenbergApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeinzenbergApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/heinzenberg/heinzenberg/HeinzenbergApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.heinzenberg.heinzenberg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeinzenbergApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
