cdb1d6407a28e872f68ee0756bbfdd2fd0d04a8c
Matheus Gonçalves <matheus.g2003@hotmail.com>
2023-09-14 19:43:42 -0300

    add dependencies add app

--- src/test/java/br/com/heinzenberg/heinzenberg/HeinzenbergApplicationTests.java
+++ src/test/java/br/com/heinzenberg/heinzenberg/HeinzenbergApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HeinzenbergApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
