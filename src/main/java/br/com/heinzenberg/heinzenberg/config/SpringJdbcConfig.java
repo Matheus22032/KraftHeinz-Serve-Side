056097d734414defa0f9b2d95026a69813aa6a8a
Matheus Gonçalves <matheus.g2003@hotmail.com>
2023-09-18 21:31:51 -0300

    jdbc configured

---
+++ src/main/java/br/com/heinzenberg/heinzenberg/config/SpringJdbcConfig.java
@@ -0,0 +1,29 @@
+package br.com.heinzenberg.heinzenberg.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJdbcRepositories
+public class SpringJdbcConfig extends AbstractJdbcConfiguration {
+    @Bean
+    public DataSource oracleDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        dataSource.setUrl("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL");
+        dataSource.setUsername("rm96166");
+        dataSource.setPassword("071203");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
