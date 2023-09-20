a3fdb854a7dac1cf730f118910cb339dcfe5d77e
Matheus Gonçalves <matheus.g2003@hotmail.com>
2023-09-20 17:55:04 -0300

    package altered

--- src/main/java/br/com/heinzenberg/heinzenberg/HeinzenbergApplication.java
+++ src/main/java/br/com/heinzenberg/heinzenberg/HeinzenbergApplication.java
@@ -1,4 +1,4 @@
-package br.com.heinzenberg.heinzenberg.app;
+package br.com.heinzenberg.heinzenberg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
