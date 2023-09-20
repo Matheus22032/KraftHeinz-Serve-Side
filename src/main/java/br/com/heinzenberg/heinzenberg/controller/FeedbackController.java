f0d2a6ed84f9693fc4d44d1a90a746513bb6215f
Matheus Gonçalves <matheus.g2003@hotmail.com>
2023-09-20 18:17:16 -0300

    method requestPerson altered to save answer

--- src/main/java/br/com/heinzenberg/heinzenberg/controller/FeedbackController.java
+++ src/main/java/br/com/heinzenberg/heinzenberg/controller/FeedbackController.java
@@ -1,5 +1,7 @@
 package br.com.heinzenberg.heinzenberg.controller;
 
+import br.com.heinzenberg.heinzenberg.entities.Person;
+import br.com.heinzenberg.heinzenberg.repository.AnswerRepository;
 import br.com.heinzenberg.heinzenberg.repository.PersonRepository;
 import br.com.heinzenberg.heinzenberg.request.PersonRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +12,21 @@ import org.springframework.web.bind.annotation.*;
 public class FeedbackController {
 
     private final PersonRepository personRepository;
+    private final AnswerRepository answerRepository;
 
     @Autowired
-    public FeedbackController(PersonRepository personRepository) {
+    public FeedbackController(PersonRepository personRepository, AnswerRepository answerRepository) {
         this.personRepository = personRepository;
+        this.answerRepository = answerRepository;
     }
 
     @PostMapping()
     public void requestPerson(@RequestBody PersonRequest personRequest){
-        personRepository.save(personRequest);
+        Person person = new Person(personRequest);
+        personRepository.save(person);
+        personRequest.getQuestions().forEach(answer -> {
+            answerRepository.saveAnswer(answer);
+        });
     }
 
 }
