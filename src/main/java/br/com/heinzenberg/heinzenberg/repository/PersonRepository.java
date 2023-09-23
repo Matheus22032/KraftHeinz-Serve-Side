48154e0c1d6710a23a2179dfdcc3f3e1f4aa9d52
Matheus Gonçalves <matheus.g2003@hotmail.com>
2023-09-22 23:24:40 -0300

    method save altered and new method findById

--- src/main/java/br/com/heinzenberg/heinzenberg/repository/PersonRepository.java
+++ src/main/java/br/com/heinzenberg/heinzenberg/repository/PersonRepository.java
@@ -1,29 +1,56 @@
 package br.com.heinzenberg.heinzenberg.repository;
 
-
 import br.com.heinzenberg.heinzenberg.entities.Person;
-import br.com.heinzenberg.heinzenberg.request.PersonRequest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Repository
 public class PersonRepository {
     private final JdbcTemplate jdbcTemplate;
 
-    private final String sqlPerson = "INSERT INTO tb_person (person_id, name_person, local_person, email, tb_questionary_questionary_id) VALUES (?, ?, ?, ?, ?)";
-    private final String sqlComment = "INSERT INTO tb_comment (comment_id, comment_text, tb_person_person_id) VALUES (?, ?, ?)";
+    private final String sqlPerson = "INSERT INTO tb_person (person_id, name_person, local_person, email, comment_person) VALUES (?, ?, ?, ?,?)";
+    private final String sqlSelectPerson = "SELECT * FROM tb_person where comment_person is not null";
+    private final String sqlSelectPersonById = "SELECT * FROM tb_person where person_id = ?";
 
     public PersonRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     public void save(Person person) {
-        jdbcTemplate.update(sqlPerson, person.getPersonId().toString(), person.getName(), person.getLocal(), person.getEmail(), 1);
-        if (person.getComment() != null) {
-            jdbcTemplate.update(sqlComment, UUID.randomUUID().toString(), person.getComment(), person.getPersonId().toString());
-        }
+        jdbcTemplate.update(sqlPerson, person.getPersonId().toString(), person.getName(), person.getLocal(), person.getEmail(), person.getComment());
+    }
 
+    public List<Person> findAll() {
+        List<Person> personList = new ArrayList<>();
+        jdbcTemplate.query(sqlSelectPerson, (rs) -> {
+            while (rs.next()) {
+                Person person = new Person(rs.getString("name_person"),
+                        rs.getString("local_person"),
+                        rs.getString("email"),
+                        rs.getString("comment_person"),
+                        UUID.fromString(rs.getString("person_id")));
+                personList.add(person);
+            }
+        });
+
+        return personList;
+    }
+
+    public Person findById(String id) {
+        AtomicReference<Person> person = new AtomicReference<>(new Person());
+        jdbcTemplate.query(sqlSelectPersonById, (rs) -> {
+                person.set(new Person(rs.getString("name_person"),
+                        rs.getString("local_person"),
+                        rs.getString("email"),
+                        rs.getString("comment_person"),
+                        UUID.fromString(rs.getString("person_id"))));
+        }, id);
+
+        return person.get();
     }
 }
