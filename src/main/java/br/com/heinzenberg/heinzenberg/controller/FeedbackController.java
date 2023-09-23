5337a1089fdced61d1b5705577a7dd5d7421fbba
Matheus Gonçalves <matheus.g2003@hotmail.com>
2023-09-22 23:11:57 -0300

    endpoin /person and person/id done

--- src/main/java/br/com/heinzenberg/heinzenberg/controller/FeedbackController.java
+++ src/main/java/br/com/heinzenberg/heinzenberg/controller/FeedbackController.java
@@ -3,9 +3,13 @@ package br.com.heinzenberg.heinzenberg.controller;
 import br.com.heinzenberg.heinzenberg.entities.Person;
 import br.com.heinzenberg.heinzenberg.repository.AnswerRepository;
 import br.com.heinzenberg.heinzenberg.repository.PersonRepository;
-import br.com.heinzenberg.heinzenberg.request.PersonRequest;
+import br.com.heinzenberg.heinzenberg.entities.PersonRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/feedback")
@@ -24,9 +28,26 @@ public class FeedbackController {
     public void requestPerson(@RequestBody PersonRequest personRequest){
         Person person = new Person(personRequest);
         personRepository.save(person);
-        personRequest.getQuestions().forEach(answer -> {
-            answerRepository.saveAnswer(answer);
+        personRequest.getQuestions().forEach((answer) -> {
+            answerRepository.saveAnswer(answer, person.getPersonId().toString());
         });
+    }
+
+    @GetMapping("/person")
+    @ResponseBody
+    public List<Person> responsePerson(){
+        return personRepository.findAll();
+    }
+
+    @GetMapping("/person/{id}")
+    @ResponseBody
+    public PersonRequest responsePerson(@PathVariable String id){
+        Person person = personRepository.findById(id);
+        if (person == null){
+            return null;
+        }
+        PersonRequest personRequest = new PersonRequest(person);
+        return answerRepository.requestAnswer(id, personRequest);
     }
 
 }
